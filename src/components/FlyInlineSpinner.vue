2331cd8c955527646c98aceb50d5fc7616b95f8e
Saïd Tayebi <said@fishtripr.com>
2018-06-01 09:32:27 +0200

    feat(): add fly-inline-spinner

---
+++ src/components/FlyInlineSpinner.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class='loader'>
+    <beat-loader :size='size' />
+  </div>
+</template>
+
+<script>
+  import BeatLoader from 'vue-spinner/src/BeatLoader.vue'
+
+  export default {
+    name: 'base-inline-spinner',
+    props: {
+      size: {
+        type: String,
+        default: '10px'
+      }
+    },
+    components: {
+      BeatLoader
+    }
+  }
+</script>
+
+
+<style lang='scss'>
+  .loader {
+    display: inline-block;
+  }
+</style>

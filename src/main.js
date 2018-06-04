8882727ed364ecc87069c5f5bb8defe275ddb44a
Saïd Tayebi <said@fishtripr.com>
2018-06-04 17:02:12 +0200

    feat(): add fly-select component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
+import App from './App.vue'
+import FishtriprUI from './components'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+Vue.use(FishtriprUI)
 
 new Vue({
   render: h => h(App),
